fa172c330c2c3bc517c065441c89a778e09bc809
Hardik Gupta <44974067+avadam21@users.noreply.github.com>
2019-08-18 08:46:50 +0530

    Add files via upload

---
+++ Support Vector Machine.py
@@ -0,0 +1,76 @@
+#Importing libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import  ListedColormap
+
+#Importing dataset
+
+File_path = 'D:\Hacking\DataSet for ML\Kernel_SVM\Social_Network_Ads.csv'
+dataset = pd.read_csv(File_path)
+X = dataset.iloc[:, [2, 3]].values
+Y = dataset.iloc[:, 4]
+
+#Feature Scalling
+
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+
+#Sppliting the dataset into test and train
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size= 0.25, random_state= 0)
+
+#Fitting SVM to the training dataset
+
+classifier = SVC(kernel= 'linear', random_state= 0)
+classifier.fit(X_train, Y_train)
+
+#Predicting the test set result
+
+Y_pred = classifier.predict(X_test)
+
+#Making the Confusion matrix
+
+cm = confusion_matrix(Y_test, Y_pred)
+print(cm)
+
+# Visualising the Training set results
+
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('orange', 'purple'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
